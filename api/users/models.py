02e06346873f4950a76e94260179c5f3ed46480c
AdalbertoCV <34152734@uaz.edu.mx>
2025-06-03 20:30:53 -0600

    Primera versión modelo Usuario

--- api/users/models.py
+++ api/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    foto_perfil = models.ImageField(upload_to='usuarios/fotos/', blank=True, null=True),
+    descripcion = models.TextField(blank=True)
+    pasatiempos = models.TextField(blank=True)
+
+
+    def __str__(self):
+        return self.username
