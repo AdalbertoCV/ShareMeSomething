6f01e1a022ccf3568adbc0620a6c8678ae98b71b
AdalbertoCV <34152734@uaz.edu.mx>
2025-06-11 12:34:59 -0600

    obtener share por id

--- api/shares/views.py
+++ api/shares/views.py
@@ -16,37 +16,45 @@ class ShareApiView(APIView):
 
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request, id=None):
         """ 
         Método get para obtener la lista de shares ya sea compartidos o recibidos
 
         filtros: fecha.
         """
-        tipo = request.query_params.get('tipo', 'recibidos')
-        fecha = request.query_params.get('fecha')  # YYYY-MM-DD 
-        usuario_id = request.query_params.get('usuario_id')  
-
-        if tipo.lower() == "recibidos":
-            queryset = Share.objects.filter(destinatario=request.user)
-            if usuario_id:
-                queryset = queryset.filter(remitente_id=usuario_id)
-
-        elif tipo.lower() == "compartidos":
-            queryset = Share.objects.filter(remitente=request.user)
-            if usuario_id:
-                queryset = queryset.filter(destinatario_id=usuario_id)
+        if id:
+            share = get_object_or_404(Share, id = id)
+            if share.remitente == request.user or share.destinatario == request.user:
+                serializer = ShareSerializer(share, context={'request':request})
+                return Response(serializer.data, status = status.HTTP_200_OK)
+            else:
+                return Response({'detail':'No tienes permisos para realizar esta acción.'},status = status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"detail": "El parámetro 'tipo' debe ser 'recibidos' o 'compartidos'."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if fecha:
-            try:
-                fecha_parsed = datetime.strptime(fecha, "%Y-%m-%d").date()
-                queryset = queryset.filter(fecha_creacion__date=fecha_parsed)
-            except ValueError:
-                return Response({"detail": "El formato de fecha debe ser YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
+            tipo = request.query_params.get('tipo', 'recibidos')
+            fecha = request.query_params.get('fecha')  # YYYY-MM-DD 
+            usuario_id = request.query_params.get('usuario_id')  
 
-        serializer = ShareSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+            if tipo.lower() == "recibidos":
+                queryset = Share.objects.filter(destinatario=request.user)
+                if usuario_id:
+                    queryset = queryset.filter(remitente_id=usuario_id)
+
+            elif tipo.lower() == "compartidos":
+                queryset = Share.objects.filter(remitente=request.user)
+                if usuario_id:
+                    queryset = queryset.filter(destinatario_id=usuario_id)
+            else:
+                return Response({"detail": "El parámetro 'tipo' debe ser 'recibidos' o 'compartidos'."}, status=status.HTTP_400_BAD_REQUEST)
+
+            if fecha:
+                try:
+                    fecha_parsed = datetime.strptime(fecha, "%Y-%m-%d").date()
+                    queryset = queryset.filter(fecha_creacion__date=fecha_parsed)
+                except ValueError:
+                    return Response({"detail": "El formato de fecha debe ser YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = ShareSerializer(queryset, many=True, context={'request': request})
+            return Response(serializer.data)
     
     def post(self, request):
         """
@@ -77,4 +85,4 @@ class ShareApiView(APIView):
             else:
                 return Response({'detail':'No tienes permisos para realizar esta acción.'},status = status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({'detail':'El id del Share es requerido para su actualización'},status = status.HTTP_400_BAD_REQUEST)
+            return Response({'detail':'El id del Share es requerido para su actualización'},status = status.HTTP_400_BAD_REQUEST)   
